ea33012ba90f4042ff36b655f35cf3fbe247ecdf
Eduardo Ortiz Passos <ortizpassos@yahoo.com.br>
2025-09-11 00:21:59 -0300

    Atualização Whatsapp

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Pagina } from "./pagina/pagina";
+import { Navbar } from "./navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [Pagina],
+  imports: [Pagina, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
