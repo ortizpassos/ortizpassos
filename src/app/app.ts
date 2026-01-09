15aaefca2b0bbd7bc69749ce1664a13030a0083e
Eduardo Ortiz Passos <ortizpassos@yahoo.com.br>
2026-01-08 22:51:52 -0300

    Whatsapp

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WhatsappButtonComponent } from './whatsapp-button/whatsapp-button';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WhatsappButtonComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/whatsapp-button/whatsapp-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whatsapp-button',
+  standalone: true,
+  templateUrl: './whatsapp-button.html',
+  styleUrl: './whatsapp-button.css'
+})
+export class WhatsappButtonComponent {
+
+}
