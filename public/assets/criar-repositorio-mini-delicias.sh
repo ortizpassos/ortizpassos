630dcd76f4a54fd1e0ffb0ec725fe416f086f607
Eduardo Ortiz Passos <ortizpassos@yahoo.com.br>
2026-01-09 00:53:02 -0300

    Oferta Especial

--- public/assets/criar-repositorio-mini-delicias.sh
+++
@@ -1,8 +0,0 @@
-cd "C:\projetos\mini-delicias"
-git init
-git add .
-git commit -m "Criação do repositório"
-git remote add origin https://github.com/ortizpassos/mini-delicias.git
-gh repo create mini-delicias --public --source=. --push --description "Projetos com mini-delicias"
-git branch -M main
-git push -u origin main
